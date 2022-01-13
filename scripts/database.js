f1a17b8b8d3a4b843e26c9ec8767c3bd6675ea03
David Clark <david.clark.1043@gmail.com>
2022-01-13 11:40:20 -0600

    Restyled database.js

--- scripts/database.js
+++ scripts/database.js
@@ -36,3 +36,43 @@ const database = {
 export const getFish = () => {
     return database.fish.map(fish => ({...fish}))
 }
+
+export const mostHolyFish = () => {
+    // 3, 6, 9, 12, etc... fish
+    // get all fish list and create array for holyfish
+    const fishes = getFish()
+    const holyFish = []
+    // filter fish of length that is a multiple of 3 and push to holyfish array
+    for (const fish of fishes) {
+        if(fish.length % 3 === 0){
+            holyFish.push(fish)
+        }
+    }
+
+    return holyFish
+}
+
+export const soldierFish = () => {
+    // 5, 10, 15, 20, 25, etc... fish
+    const fishes = getFish()
+    const soldiers = []
+    for (const fish of fishes) {
+        if(fish.length % 5 === 0){
+            soldiers.push(fish)
+        }
+    }
+
+    return soldiers
+}
+
+export const nonHolyFish = () => {
+    // Any fish not a multiple of 3 or 5
+    const fishes = getFish()
+    const regularFish = []
+    for (const fish of fishes) {
+        if(fish.length % 3 != 0 && fish.length % 5 != 0) {
+            regularFish.push(fish)
+        }
+    }
+    return regularFish
+}
--- scripts/fish/FishList.js
+++ scripts/fish/FishList.js
@@ -1,5 +1,7 @@
 // Import the function that returns a copy of the fish array
-import { getFish } from './FishDatabase.js'
+
+import { mostHolyFish, nonHolyFish, soldierFish } from "../database.js"
+
 
 export const FishList = () => {
     // Get the filtered lists of fish
@@ -49,42 +51,3 @@ export const FishList = () => {
     return htmlString
 }
 
-export const mostHolyFish = () => {
-    // 3, 6, 9, 12, etc... fish
-    // get all fish list and create array for holyfish
-    const fishes = getFish()
-    const holyFish = []
-    // filter fish of length that is a multiple of 3 and push to holyfish array
-    for (const fish of fishes) {
-        if(fish.length % 3 === 0){
-            holyFish.push(fish)
-        }
-    }
-
-    return holyFish
-}
-
-export const soldierFish = () => {
-    // 5, 10, 15, 20, 25, etc... fish
-    const fishes = getFish()
-    const soldiers = []
-    for (const fish of fishes) {
-        if(fish.length % 5 === 0){
-            soldiers.push(fish)
-        }
-    }
-
-    return soldiers
-}
-
-export const nonHolyFish = () => {
-    // Any fish not a multiple of 3 or 5
-    const fishes = getFish()
-    const regularFish = []
-    for (const fish of fishes) {
-        if(fish.length % 3 != 0 && fish.length % 5 != 0) {
-            regularFish.push(fish)
-        }
-    }
-    return regularFish
-}
